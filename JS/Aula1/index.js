ffbc2640954b78f2f1f85034f2cd177b362f489c
Romario <romarioinfobraz@gmail.com>
2025-10-22 04:37:56 -0300

    Aula 3
    Continuação das aulas de html, vimos spam, div, imagens e links.

---
+++ JS/Aula1/index.js
@@ -0,0 +1,16 @@
+console.log('Olá Mundo.');
+console.log("Olá 'Mundo2'.");
+console.log(548756, 456464.15);
+console.log(548756, "Romario Braz");
+console.log("Meu nome é Romário, e tenho",31,"Anos");
+//comentários de uma linha
+/*
+Agora temos um bloco de comentários com várias linhas
+*/
+
+/**
+ * Titulo
+ * 
+ * Temos um texto que pode ser mais longo.
+ * Fim.
+ */
